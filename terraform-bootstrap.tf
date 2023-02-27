7877192029a057e98a49cfaf9984af4774cd6f52
A. R. Younce <ayounce@ary.pub>
2023-02-26 19:56:26 -0700

    Stub out Terraform equivalent to the existing CloudFormation stack.

---
+++ terraform-bootstrap.tf
@@ -0,0 +1,78 @@
+/*
+ * All variables are optional. It should be noted that if you do not provide a
+ * name for the S3 bucket then Terraform will assign a random, unique name. See;
+ * https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket#bucket
+ */
+
+variable "aws_region" {
+  type    = string
+  default = null
+}
+
+variable "s3_bucket_name" {
+  type = string
+}
+
+variable "dynamo_table_name" {
+  type    = string
+  default = "terraform-locking"
+}
+
+variable "parameter_prefix" {
+  type    = string
+  default = "terraform"
+}
+
+/*
+ * The 'aws' provider will use the various `AWS_*` environment variables expected
+ * by the AWS CLI and SDKs. See;
+ * https://registry.terraform.io/providers/hashicorp/aws/latest/docs#environment-variables
+ * https://registry.terraform.io/providers/hashicorp/aws/latest/docs#aws-configuration-reference
+ *
+ * Modify this section as needed.
+ */
+provider "aws" {}
+
+resource "aws_s3_bucket" "terraform_state_bucket" {
+  bucket = can(var.s3_bucket_name) ? var.s3_bucket_name : null
+
+  tags = {
+    Name        = "Terraform S3 Backend - State"
+    application = "Terraform S3 Backend"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock_table" {
+  name           = var.dynamo_table_name
+  hash_key       = "LockID"
+
+  read_capacity  = 1
+  write_capacity = 1
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform S3 Backend - Locking"
+    application = "Terraform S3 Backend"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes = [ "read_capacity", "write_capacity" ]
+  }
+}
