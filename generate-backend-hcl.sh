8a526b2d4550f9148283b8d66838b8f68545f55e
Adam Younce <ayounce@ary.pub>
2023-03-11 15:30:34 -0700

    Refactor `generate-backend-hcl.sh` to use Parameter Store values.

--- generate-backend-hcl.sh
+++ generate-backend-hcl.sh
@@ -1,49 +1,69 @@
 #!/bin/bash
 #
-# Generate Terraform HCL for a S3 backend created with CloudFormation.
+# Generate Terraform HCL for a S3 backend.
 #
-# Use this to create backend configuration HCL based on the deployed bootstrap
-# stack in 'terraform-bootstrap.yaml'. This script will assume you used the
-# stack name in the example AWS CLI deployment command from the README.md.
+# Use this to create backend configuration HCL based on the deployed backend.
+# This script will look in the AWS account to which your active credentials
+# belong to.
 #
 # In case you need to use this for multiple state files or you have deployed
-# mutiple S3 backends you can specify the state file key name as the first
-# argument to this script, and the CloudFormation stack name as the second
-# argument to this script.
+# mutiple S3 backends you can specify the state name as the first argument to
+# this script, and the SSM Parameter Store path prefix as the second argument
+# to this script.
 #
-# Usage: ./generate-backend-conf.sh [state-key-name] [cloudformation-stack-name]
+# Usage: ./generate-backend-conf.sh [state-name] [ssm-param-path-prefix]
 #
 
-[[ $(which jq) ]] || \
-  {
-    echo "This script requires the 'jq' command. See more @ https://stedolan.github.io/jq/" >> /dev/stderr
-    exit 1
-  }
+set -eu -o pipefail
+
+[[ $(which jq) ]] || {
+  echo "This script requires the 'jq' command. See more @ https://stedolan.github.io/jq/" >> /dev/stderr
+  exit 1
+}
+
+[[ $(which terraform) ]] || {
+  echo "This script requires the 'terraform' command."
+  exit 1
+}
 
 echo "# Generating Terraform HCL for S3 backend" >> /dev/stderr
+echo "# > Usage: $0 [state-name] [ssm-param-path-prefix]"
 
-# TODO Document
-state_key_default="terraform"
-state_key="${1-$state_key_default}"
+# Name of the state object in S3. Will have a .tfstate suffix in the bucket.
+state_name_default="terraform"
+state_name=${1-$state_name_default}
 
-[[ "${state_key}" == "${state_key_default}" ]] && \
-  echo "# Using default Terraform state key: '${state_key}'" >> /dev/stderr
+[[ "${state_name}" == "${state_name_default}" ]] && \
+  echo "# > Using default Terraform state name: '${state_name}'" >> /dev/stderr
 
-# TODO Document
-stack_name_default="terraform-bootstrap"
-stack_name="${2-$stack_name_default}"
-[[ "${stack_name}" == "${stack_name_default}" ]] && \
-  echo "# Using default CloudFormation stack name: '${stack_name}'" >> /dev/stderr
+# SSM Parameter Store path prefix (for locating values needed by this script)
+param_prefix_default="terraform"
+param_prefix="$(echo ${2-${param_prefix_default}} | sed -E 's#^/*(.*[^/])/?/*$#\1#')"
 
-aws cloudformation describe-stacks --stack-name "${stack_name}" | \
-jq -r --arg state_key "${state_key}" $'"terraform {
+[[ "${param_prefix}" == "${param_prefix_default}" ]] && \
+  echo "# > Using default Terraform SSM parameter path: '${param_prefix}'" >> /dev/stderr
+
+echo "# Reading parameters from SSM Parameter Store prefix: /${param_prefix}/" >> /dev/stderr
+
+#
+# Currently this only check that the number of returned Parameter Store values
+# is greater than zero. Whether or not the expected parameters are returned is
+# not (yet) handled gracefully.
+#
+aws ssm get-parameters-by-path --output json --path "/${param_prefix}/" \
+| jq -r --arg state_name "${state_name}" --arg param_prefix "${param_prefix}" \
+$'if (.Parameters | length) > 0
+then .Parameters | "terraform {
   backend \\"s3\\" {
-    region         = \\"\(.Stacks[0] | .StackId | split(":")[3])\\"
-    bucket         = \\"\(.Stacks[0] | .Outputs[] | select(.OutputKey == "Bucket") | .OutputValue)\\"
-    key            = \\"\(.Stacks[0] | .Parameters[] | select(.ParameterKey == "S3StatePrefix") | .ParameterValue)/\($state_key).tfstate\\"
-    dynamodb_table = \\"\(.Stacks[0] | .Parameters[] | select(.ParameterKey == "DynamoDbTableName") | .ParameterValue)\\"
+    bucket         = \\"\(.[] | select(.Name | test(\"/s3-backend-bucket$\")) | .Value)\\"
+    key            = \\"\(.[] | select(.Name | test(\"/s3-backend-prefix$\")) | .Value)/\($state_name).tfstate\\"
+    dynamodb_table = \\"\(.[] | select(.Name | test(\"/s3-backend-lock-table$\")) | .Value)\\"
+
     # Use of S3 bucket encryption must be enable by the user.
     #encrypt        = true
     #kms_key_id     = \\"alias/terraform-bucket-key\\"
- }
-}"'
+  }
+}"
+else
+  "Error - No SSM Parameter Store values found at: /\($param_prefix)/"
+end'
